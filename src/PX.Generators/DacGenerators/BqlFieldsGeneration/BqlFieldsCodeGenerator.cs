4e65703624e9002554bb4b663a94cd349aadd8fe
Alexey Kuznetsov <noofsaeidh@outlook.com>
2023-01-07 12:49:47 +0100

    clear up

--- src/PX.Generators/DacGenerators/BqlFieldsGeneration/BqlFieldsCodeGenerator.cs
+++ src/PX.Generators/DacGenerators/BqlFieldsGeneration/BqlFieldsCodeGenerator.cs
@@ -75,44 +75,5 @@ namespace PX.Generators.DacGenerators.BqlFieldsGeneration
                                        .Where(f => f != null)!));
             }
         }
-
-
-        public (string? FileName, string? Code) Generate_(BqlTableInfo bqlTable)
-        {
-            if (bqlTable.Fields?.Count is not > 0)
-                return default;
-
-            bool fieldAdded = false;
-            var  cb         = new CodeBuilder(Indentation);
-            using (string.IsNullOrEmpty(bqlTable.Namespace)
-                       ? cb.EmptyIndentation
-                       : cb.AppendLineWithBrackets($"namespace {bqlTable.Namespace}"))
-            {
-                using (cb.AppendLineWithBrackets($"partial class {bqlTable.Name}"))
-                {
-                    foreach (var bqlField in bqlTable.Fields)
-                    {
-                        if (BqlFieldClassType.TryMap(bqlField.Type, out var className) is false)
-                            continue;
-
-                        string addNew = bqlField.IsHidingBaseClass ? "new" : string.Empty;
-                        cb.AppendLine(
-                            $"public static {addNew} class : PX.Data.BQL.{className}.Field<{className}> {{ }}");
-                        fieldAdded = true;
-                    }
-                }
-
-                cb.Append("ha1");
-                cb.AppendLine("ha2");
-            }
-
-            cb.Append("ha3");
-            cb.AppendLine("ha4");
-
-            if (fieldAdded is false)
-                return default;
-
-            return ($"{bqlTable.FullName}.g.cs", cb.ToString());
-        }
     }
 }
